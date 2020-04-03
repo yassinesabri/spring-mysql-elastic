3dd513067d1cb335d01851849b87b5396697e703
Yassine SABRI <ysabri@leyton.com>
2020-04-03 18:34:51 +0200

    initializing project

---
+++ src/main/java/com/example/springmysqlelastic/SpringMysqlElasticApplication.java
@@ -0,0 +1,14 @@
+package com.example.springmysqlelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlElasticApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringMysqlElasticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmysqlelastic/SpringMysqlElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmysqlelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlElasticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
